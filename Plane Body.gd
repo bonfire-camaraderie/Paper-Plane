ea5ed6655a5edc9e7392ba66d8353b3ae7629cb0
bonfire-camaraderie <shahsi777>
2020-02-20 04:19:05 +0100

    added horizontal control and collision bars and at the left and right boundaries of the game window

--- Plane Body.gd
+++ Plane Body.gd
@@ -1,14 +1,32 @@
 extends KinematicBody2D
 
-const STEP = 200
+export var STEP = 200
 var motion = Vector2()
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
-		motion.y = -STEP
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = STEP
-	else:
+	var up = Input.is_action_pressed("ui_up")
+	var down = Input.is_action_pressed("ui_down")
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	
+	match true:
+		up :
+			motion.y = -STEP
+			continue
+		down :
+			motion.y = STEP
+			continue
+		left :
+			motion.x = -STEP
+			continue
+		right :
+			motion.x = STEP
+			continue
+			
+	if up == false and down == false :
 		motion.y = 0
+		
+	if left == false and right == false :
+		motion.x = 0
 	
 	move_and_slide(motion)
